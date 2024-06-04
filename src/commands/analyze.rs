09a4a43268eef0fb392d7d6c4340de58d300e0b9
Andrei Zhigalkin <snacks64@proton.me>
2024-06-04 23:46:09 +0500

    Implement the `analyze` command

--- src/commands/analyze.rs
+++ src/commands/analyze.rs
@@ -1,11 +1,119 @@
 use anyhow::Error;
 use clap::Parser;
+use std::collections;
+use std::fs;
+
+use crate::measurement_parser;
+
+mod database;
 
 #[derive(Debug, Parser)]
-pub(crate) struct Command {}
+pub(crate) struct Command {
+    /// Set the input file
+    #[arg(default_value = "squig.sqlite3", long, short)]
+    input: String,
+
+    /// Set the output directory
+    #[arg(default_value = "out", long, short)]
+    output: String,
+}
+
+struct Bar {
+    brand_name: String,
+    average_channel_imbalance: f64,
+}
 
 impl Command {
     pub fn execute(&self) -> Result<(), Error> {
+        let mut connection = rusqlite::Connection::open(&self.input)?;
+        fs::create_dir_all(&self.output)?;
+
+        let transaction = connection.transaction()?;
+        let mut brand_differences = collections::HashMap::new();
+        for output in database::channel_imbalance::select(&transaction)? {
+            let values = brand_differences
+                .entry(output.brand_name)
+                .or_insert(Vec::<f64>::new());
+            let left_channel_points = measurement_parser::parse(&output.left_channel_text)?;
+            let right_channel_points = measurement_parser::parse(&output.right_channel_text)?;
+            if left_channel_points
+                .iter()
+                .zip(&right_channel_points)
+                .any(|(l, r)| l.frequency_hz != r.frequency_hz)
+            {
+                continue;
+            }
+            let left_channel_average = left_channel_points
+                .iter()
+                .map(|point| point.spl_db)
+                .sum::<f64>()
+                / left_channel_points.len() as f64;
+            let right_channel_average = right_channel_points
+                .iter()
+                .map(|point| point.spl_db)
+                .sum::<f64>()
+                / right_channel_points.len() as f64;
+            values.push((left_channel_average - right_channel_average).abs());
+        }
+        let mut brand_differences: Vec<Bar> = brand_differences
+            .iter()
+            .map(|(name, points)| {
+                let average_channel_imbalance = points.iter().sum::<f64>() / points.len() as f64;
+                Bar {
+                    brand_name: format!(
+                        "{} ({}) (~{:.2} dB)",
+                        name,
+                        points.len(),
+                        average_channel_imbalance
+                    ),
+                    average_channel_imbalance,
+                }
+            })
+            .collect();
+        brand_differences.sort_by(|a, b| {
+            a.average_channel_imbalance
+                .total_cmp(&b.average_channel_imbalance)
+        });
+        let keys: Vec<String> = brand_differences
+            .iter()
+            .map(|brand_difference| brand_difference.brand_name.clone())
+            .collect();
+        let values: Vec<f64> = brand_differences
+            .iter()
+            .map(|brand_difference| brand_difference.average_channel_imbalance)
+            .collect();
+        let mut plot = plotly::Plot::new();
+        plot.add_trace(
+            plotly::Bar::new(values.clone(), keys.clone())
+                .marker(
+                    plotly::common::Marker::new()
+                        .color(plotly::common::color::Rgb::new(58, 124, 191)),
+                )
+                .orientation(plotly::common::Orientation::Horizontal)
+                .show_legend(false),
+        );
+        plot.set_configuration(
+            plotly::Configuration::new()
+                .display_mode_bar(plotly::configuration::DisplayModeBar::False)
+                .responsive(true),
+        );
+        plot.set_layout(
+            plotly::Layout::new()
+                .bar_gap(0.1)
+                .bar_mode(plotly::layout::BarMode::Stack)
+                .drag_mode(plotly::layout::DragMode::False)
+                .height(keys.len() * 30)
+                .margin(
+                    plotly::layout::Margin::new()
+                        .bottom(14)
+                        .left(200)
+                        .right(0)
+                        .top(0),
+                ),
+        );
+        plot.write_html(format!("{}/channel_imbalance.html", &self.output));
+        transaction.commit()?;
+
         Ok(())
     }
 }
---
+++ src/commands/analyze/database/channel_imbalance.rs
@@ -0,0 +1,39 @@
+use anyhow::Error;
+use indoc::indoc;
+
+#[derive(Debug)]
+pub struct Output {
+    pub brand_name: String,
+    pub left_channel_text: String,
+    pub right_channel_text: String,
+}
+
+pub fn select(transaction: &rusqlite::Transaction) -> Result<Vec<Output>, Error> {
+    let query = indoc!(
+        "
+        SELECT brands.name, left_channels.text AS left_channel_text, right_channels.text AS right_channel_text
+        FROM brands
+        JOIN phones ON phones.brand_id = brands.id
+        JOIN files ON files.phone_id = phones.id
+        JOIN channels left_channels ON left_channels.file_id = files.id
+        JOIN channels right_channels ON right_channels.file_id = left_channels.file_id
+        AND left_channels.id < right_channels.id
+        AND left_channels.idx = right_channels.idx
+        AND left_channels.text != right_channels.text
+        AND left_channels.type != right_channels.type
+        "
+    )
+    .trim_end();
+    let mut statement = transaction.prepare(query)?;
+    log::info!("{}", query);
+    let outputs = statement
+        .query_map([], |row| {
+            Ok(Output {
+                brand_name: row.get(0)?,
+                left_channel_text: row.get(1)?,
+                right_channel_text: row.get(2)?,
+            })
+        })?
+        .collect::<Result<Vec<Output>, rusqlite::Error>>()?;
+    Ok(outputs)
+}
